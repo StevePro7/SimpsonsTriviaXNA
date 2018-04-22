05bf92d0ff9f96a4ca609d3610b562475e1468b8
StevePro7 <steveprozune@hotmail.com>
2018-04-22 16:36:54 +0100

    Add system tests

---
+++ SimpsonsTrivia.XNA.SystemTests/Library/Managers/FileManagerSystemTests.cs
@@ -0,0 +1,49 @@
+﻿using WindowsGame.Implementation;
+using WindowsGame.Library.Interfaces;
+using WindowsGame.Library.Managers;
+using NUnit.Framework;
+
+namespace WindowsGame.SystemTests.Library.Managers
+{
+	[TestFixture]
+	public class FileManagerSystemTests
+	{
+		private IFileManager fileManager;
+
+		[SetUp]
+		public void SetUp()
+		{
+			IFileProxy fileProxy = new RealFileProxy();
+			fileManager = new FileManager(fileProxy);
+		}
+
+		[Test]
+		public void LoadTxtTest()
+		{
+			//const String world = "01";
+			//const String round = "01";
+			//String file = String.Format("{0}{1}/{2}/{3}/{4}{5}/{6}{7}.txt", CONTENT_ROOT, Constants.CONTENT_DIRECTORY,
+			//    Constants.DATA_DIRECTORY, Constants.LEVELS_DIRECTORY, Constants.WORLD_FILENAME, world, Constants.ROUND_FILENAME,
+			//    round);
+
+			//var levelData = FileManager.LoadTxt(file);
+			//Console.WriteLine("LevelData:" + levelData.Count);
+		}
+
+		[Test]
+		public void LoadXmlTest()
+		{
+			//String file = String.Format("{0}{1}/{2}/{3}/{4}", CONTENT_ROOT, Constants.CONTENT_DIRECTORY, Constants.DATA_DIRECTORY,
+			//    Constants.CONFIG_DIRECTORY, Constants.GLOBAL_CONFIG_FILENAME);
+
+			//var configData = FileManager.LoadXml<GlobalConfigData>(file);
+			//Console.WriteLine("ScreenType:" + configData.ScreenType);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			fileManager = null;
+		}
+	}
+}
