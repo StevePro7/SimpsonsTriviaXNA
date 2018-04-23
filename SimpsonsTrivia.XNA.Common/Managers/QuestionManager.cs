21f25b00735930f2c91f879e421e8e5fc1987813
StevePro7 <steveprozune@hotmail.com>
2018-04-23 19:43:06 +0100

    Update tests

--- SimpsonsTrivia.XNA.Common/Managers/QuestionManager.cs
+++ SimpsonsTrivia.XNA.Common/Managers/QuestionManager.cs
@@ -7,23 +7,48 @@ namespace WindowsGame.Common.Managers
 {
 	public interface IQuestionManager
 	{
+		void Initialize();
+		void Initialize(String root);
 		IList<Question> LoadQuestionList(DifficultyType type);
-		Question LoadQuestion(String text);
+		Question LoadQuestion(String line);
 	}
 
 	public class QuestionManager : IQuestionManager
 	{
+		private String questionRoot;
+
 		private static char[] semicolon = new char[] { ';' };
 		private static char[] pipe = new char[] { '|' };
 
-		public IList<Question> LoadQuestionList(DifficultyType type)
+		public void Initialize()
 		{
-			return null;
+			Initialize(String.Empty);
+		}
+		public void Initialize(String root)
+		{
+			questionRoot = root;
 		}
 
-		public Question LoadQuestion(String text)
+		public IList<Question> LoadQuestionList(DifficultyType type)
 		{
-			String[] texts = text.Split(semicolon);
+			IList<Question> questionList = new List<Question>();
+
+			String file = String.Format("{0}{1}/{2}/{3}/{4}.txt", questionRoot, Constants.CONTENT_DIRECTORY, Constants.DATA_DIRECTORY,
+				Constants.LEVELS_DIRECTORY, type);
+
+			var lines = MyGame.Manager.FileManager.LoadTxt(file);
+			foreach (String line in lines)
+			{
+				Question question = LoadQuestion(line);
+				questionList.Add(question);
+			}
+
+			return questionList;
+		}
+
+		public Question LoadQuestion(String line)
+		{
+			String[] texts = line.Split(semicolon);
 
 			String[] questionText = texts[1].Split(pipe);
 			String[] answerAText = texts[2].Split(pipe);
@@ -35,5 +60,7 @@ namespace WindowsGame.Common.Managers
 			return new Question(questionText, answerAText, answerBText, answerCText, answerDText, answerCode);
 		}
 
+
+		public string CONTENT_ROOT { get; set; }
 	}
 }
