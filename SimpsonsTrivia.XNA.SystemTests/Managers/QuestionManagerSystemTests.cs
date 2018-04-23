c2b1d770233dc08699f6f9f6e1c1adf2864dfbe7
StevePro7 <steveprozune@hotmail.com>
2018-04-23 19:42:54 +0100

    Rename files

---
+++ SimpsonsTrivia.XNA.SystemTests/Managers/QuestionManagerSystemTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using WindowsGame.Common.Managers;
+using WindowsGame.Common.Objects;
+using WindowsGame.Common.Static;
+
+namespace WindowsGame.SystemTests.Managers
+{
+	[TestFixture]
+	public class QuestionManagerSystemTests : BaseSystemTests
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			QuestionManager = MyGame.Manager.QuestionManager;
+			QuestionManager.Initialize(CONTENT_ROOT);
+		}
+
+		[Test]
+		public void LoadQuestionListTest()
+		{
+			// Arrange.
+			DifficultyType type = DifficultyType.Easy;
+
+			// Act.
+			IList<Question> questionList = QuestionManager.LoadQuestionList(type);
+
+			// Assert.
+			Assert.That(3, Is.EqualTo(questionList.Count));
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			QuestionManager = null;
+		}
+
+	}
+}
---
+++ SimpsonsTrivia.XNA.UnitTests/Managers/QuestionManagerUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using WindowsGame.Common.Managers;
+using WindowsGame.Common.Objects;
+
+namespace WindowsGame.UnitTests.Managers
+{
+	[TestFixture]
+	public class QuestionManagerUnitTests : BaseUnitTests
+	{
+		[SetUp]
+		public new void SetUp()
+		{
+			QuestionManager = new QuestionManager();
+		}
+
+		[Test]
+		public void LoadQuestionDataTest()
+		{
+			String line = "3;WHICH OF THE FOLLOWING|CHARACTERS IS A NON-SMOKER?;KRUSTY;NELSON;GRAMPA SIMPSON;MRS. KRABAPPLE;page01;02-GeneralSimpsonsTrivia.csv";
+
+			Question question = QuestionManager.LoadQuestion(line);
+
+			Assert.That(question, Is.Null);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			QuestionManager = null;
+		}
+
+	}
+}
