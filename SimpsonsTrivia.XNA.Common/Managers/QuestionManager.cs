7787c42fe0674588df25fcbeda172e32f3730ede
StevePro7 <steveprozune@hotmail.com>
2018-04-23 15:19:10 +0100

    Add questions

---
+++ SimpsonsTrivia.XNA.Common/Managers/QuestionManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using WindowsGame.Common.Objects;
+using WindowsGame.Common.Static;
+
+namespace WindowsGame.Common.Managers
+{
+	public interface IQuestionManager
+	{
+		Question LoadQuestion(String text);
+	}
+
+	public class QuestionManager : IQuestionManager
+	{
+		private static char[] semicolon = new char[] { ';' };
+		private static char[] pipe = new char[] { '|' };
+
+		public Question LoadQuestion(String text)
+		{
+			String[] texts = text.Split(semicolon);
+
+			String[] questionText = BuildArray(texts[1]);
+			String[] answerAText = BuildArray(texts[2]);
+			String[] answerBText = BuildArray(texts[3]);
+			String[] answerCText = BuildArray(texts[4]);
+			String[] answerDText = BuildArray(texts[5]);
+			Byte answerCode = Convert.ToByte(texts[0]);
+
+			return new Question(questionText, answerAText, answerBText, answerCText, answerDText, answerCode);
+		}
+
+		private static String[] BuildArray(String data)
+		{
+			String[] lines = new String[Constants.NUMBER_LINES];
+
+			String[] tests = data.Split(pipe);
+			if (tests.Length > 2)
+			{
+				lines[2] = tests[2];
+			}
+			if (tests.Length > 1)
+			{
+				lines[1] = tests[1];
+			}
+
+			lines[0] = tests[0];
+			return lines;
+		}
+
+	}
+}
---
+++ SimpsonsTrivia.XNA.UnitTests/Managers/QuestionManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using WindowsGame.Common.Managers;
+using WindowsGame.Common.Objects;
+
+namespace WindowsGame.UnitTests.Managers
+{
+	public class QuestionManagerTests : BaseUnitTests
+	{
+		private IQuestionManager questionManager;
+
+		[SetUp]
+		public void SetUp()
+		{
+			questionManager = new QuestionManager();
+		}
+
+		[Test]
+		public void LoadQuestionDataTest()
+		{
+			String text = "3;WHICH OF THE FOLLOWING|CHARACTERS IS A NON-SMOKER?;KRUSTY;NELSON;GRAMPA SIMPSON;MRS. KRABAPPLE;page01;02-GeneralSimpsonsTrivia.csv";
+			//DifficultyType type = DifficultyType.Easy;
+
+			Question question = questionManager.LoadQuestion(text);
+
+			Assert.That(question, Is.Null);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			questionManager = null;
+		}
+	}
+
+}
